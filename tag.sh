d6061e3b477056959706e84407a1179e1c5f9335
HadasKBLN <hadasmazooz@gmail.com>
2021-05-11 22:16:52 +0300

    delete test.sh

---
+++ tag.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+if [ "$BRANCH_NAME" = "master" ]; then
+    tag="1.0.$BUILD_NUMBER"
+    docker tag echo:1.0 eu.gcr.io/finalexam-267908/echo:$tag
+    docker push eu.gcr.io/finalexam-267908/echo:$tag
+                            
+
+elif [ "$BRANCH_NAME" = "dev" ]; then
+    tag_tail=$(git rev-parse HEAD)
+    full_tag="dev-$tag_tail"
+    docker tag echo:1.0 eu.gcr.io/finalexam-267908/echo:$full_tag
+    docker push eu.gcr.io/finalexam-267908/echo:$full_tag
+
+else 
+    tag_tail=$(git rev-parse HEAD)
+    full_tag="staging-$tag_tail"
+    docker tag echo:1.0 eu.gcr.io/finalexam-267908/echo:$full_tag
+    docker push eu.gcr.io/finalexam-267908/echo:$full_tag
+                 
+fi 
--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-isTestsPassed=$(cat testResult.txt | grep "12 passed")
-if [[ -z "$isTestsPassed" ]]; then
-    echo 'failare'
-    exit 1
-else
-    echo '=========== E2E Test Finish with Success =============='
-fi
